00406a6ede68025f96ee2580cb1cc17f5edf470c
suppergerrie2 <gerjan2001@gmail.com>
2020-12-01 11:21:43 +0100

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,13 @@
+use aoc_utils::input_helper::Day;
+
+pub struct Day0 {}
+
+impl Day for Day0 {
+    fn part1(&self) {
+        println!("Day 0 part 1 succeeded!")
+    }
+
+    fn part2(&self) {
+        println!("Day 0 part 2 succeeded!")
+    }
+}
